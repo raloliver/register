c5979eb0872a6464ee903a5b477b913fe3008bc4
ilimaoliveira <israel.limaoliveira@altran.com>
2020-04-27 18:23:31 +0100

    Add async and await and remove pause and resume from connection

--- connect.js
+++ connect.js
@@ -13,15 +13,13 @@ function getPerson(stream) {
     connection.connect(() => {
         stream.write('name,occupation\n')
         const query = connection.query('select * from person')
-        query.on('result', (row) => {
-            connection.pause()
+        query.on('result', async (row) => {
+            // connection.pause()
             const result = row.name + ', ' + row.occupation + '\n'
-            setTimeout(() => {
-                stream.write(result, () => {
-                    connection.resume()
-                    console.log(row.id);
-                })
-            }, 1000)
+            await stream.write(result, () => {
+                // connection.resume()
+                console.log(row.id);
+            })
         });
         query.on('end', () => {
             stream.end()

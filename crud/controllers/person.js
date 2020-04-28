2143382eaf3f9ef59c890696e5db5ecd489e364b
ilimaoliveira <israel.limaoliveira@altran.com>
2020-04-28 16:31:07 +0100

    Befor e init views

--- crud/controllers/person.js
+++ crud/controllers/person.js
@@ -1,8 +1,8 @@
 const person = require('../models/person')
-
+// controller: get from somewhere and send to another where with something inside
 const index = async (connection, req, res) => {
     const people = await person.findAll(connection)
-    res.send(people)
+    res.render('people/index')
 }
 
 module.exports = {

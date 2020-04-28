793539bf005d23334f61ac9859c6ab0130714e3d
ilimaoliveira <israel.limaoliveira@altran.com>
2020-04-28 16:49:11 +0100

    Before start delete an item

--- crud/controllers/person.js
+++ crud/controllers/person.js
@@ -1,8 +1,8 @@
 const person = require('../models/person')
 // controller: get from somewhere and send to another where with something inside
 const index = async (connection, req, res) => {
-    const people = await person.findAll(connection)
-    res.render('people/index')
+    const results = await person.findAll(connection)
+    res.render('person/index', { people: results })
 }
 
 module.exports = {

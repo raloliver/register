f20e6cd9c0d82855a7a4ad5b4fe92a7e3300273d
ilimaoliveira <israel.limaoliveira@altran.com>
2020-04-28 15:30:40 +0100

    Init crud

---
+++ crud/index.js
@@ -0,0 +1,15 @@
+const env = require('./.env')
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || env.port
+
+// static files
+app.use(express.static('public'))
+
+// view engine 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// start server 
+app.listen(port, () => console.log('CRUD it is on port', port))

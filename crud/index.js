1a2cf8664e3550195faf0b2a13edee7d2d245c70
ilimaoliveira <israel.limaoliveira@altran.com>
2020-04-28 15:43:51 +0100

    Add bootstrap 4 with ejs way to project

--- crud/index.js
+++ crud/index.js
@@ -7,6 +7,9 @@ const port = process.env.PORT || env.port
 // static files
 app.use(express.static('public'))
 
+// routes
+app.get('/', (req, res) => res.render('index'))
+
 // view engine 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
